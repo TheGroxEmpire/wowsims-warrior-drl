a2b8034ff18d6c76c822bdc59b2a0d33986dfc3a
b-r-a-n <brandon.cole.smith@gmail.com>
2023-04-28 09:20:04 -0700

    Update the example notebook to include more use cases

--- feral.py
+++ feral.py
@@ -3,6 +3,14 @@ import ctypes
 import wowsims
 
 class Spells():
+    Berserk = None
+    Bite = None
+    FFF = None
+    Fury = None
+    Mangle = None
+    Rake = None
+    Roar = None
+    Rip = None
     Shred = None
 
     @classmethod
@@ -13,4 +21,52 @@ class Spells():
         spells_ptr = (ctypes.c_int * len(spells)).from_buffer(spells)
         wowsims.getSpells(spells_ptr, len(spells))
         for i, spell in enumerate(spells):
+            if spell == 50334 and cls.Berserk is None: cls.Berserk = i
+            if spell == 48577 and cls.Bite is None: cls.Bite = i
+            if spell == 16857 and cls.FFF is None: cls.FFF = i
+            if spell == 50213 and cls.Fury is None: cls.Fury = i
+            if spell == 71925 and cls.Mangle is None: cls.Mangle = i
+            if spell == 48574 and cls.Rake is None: cls.Rake = i
+            if spell == 52610 and cls.Roar is None: cls.Roar = i
+            if spell == 49800 and cls.Rip is None: cls.Rip = i
             if spell == 48572 and cls.Shred is None: cls.Shred = i
+
+class Auras():
+    Labels = ["Berserk", "Cat Form", "Clearcasting", "Savage Roar Aura", "Tiger's Fury Aura"]
+    Durations = array.array('d', [0.0] * len(Labels))
+
+    @classmethod
+    def register(cls):
+        ptr = (ctypes.c_char_p * (len(cls.Labels)+1))()
+        ptr[:-1] = [label.encode('utf-8') for label in cls.Labels]
+        ptr[-1] = None
+        wowsims.registerAuras(ptr)
+    
+    @classmethod
+    def update(cls):
+        ptr = (ctypes.c_double * len(cls.Durations)).from_buffer(cls.Durations)
+        wowsims.getAuras(ptr, len(cls.Durations))
+    
+    @classmethod
+    def get_dur(cls, label):
+        return cls.Durations[cls.Labels.index(label)]
+
+class TargetAuras():
+    Labels = ["Rake", "Rip", "Mangle"]
+    Durations = array.array('d', [0.0] * len(Labels))
+
+    @classmethod
+    def register(cls):
+        ptr = (ctypes.c_char_p * (len(cls.Labels)+1))()
+        ptr[:-1] = [label.encode('utf-8') for label in cls.Labels]
+        ptr[-1] = None
+        wowsims.registerTargetAuras(ptr)
+    
+    @classmethod
+    def update(cls):
+        ptr = (ctypes.c_double * len(cls.Durations)).from_buffer(cls.Durations)
+        wowsims.getTargetAuras(ptr, len(cls.Durations))
+
+    @classmethod
+    def get_dur(cls, label):
+        return cls.Durations[cls.Labels.index(label)]
